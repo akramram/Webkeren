62b21700797fb7919a46d09702c8ba0163ac3732
ata45 <34927815+ata45@users.noreply.github.com>
2018-10-23 14:28:02 +0700

    Update index.php
    nih

--- index.php
+++ index.php
@@ -2,39 +2,39 @@
 <html>
 <head>
 	<title></title>
-  <link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-<div class="col-12">
+	<div class="col-12">
 
-  <div class="col-3">
-    <h1>Sorting Ascending</h1>
-    <?php
-    $cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
-    sort($cars);
+		<div class="col-3">
+			<h1>Sorting Ascending</h1>
+			<?php
+			$cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
+			sort($cars);
 
-    $clength = count($cars);
-    for($x = 0; $x < $clength; $x++) {
-        echo $cars[$x];
-        echo "<br>";
-    }
-    ?>
-  </div>
+			$clength = count($cars);
+			for($x = 0; $x < $clength; $x++) {
+				echo $cars[$x];
+				echo "<br>";
+			}
+			?>
+		</div>
 
-  <div class="col-3">
-    <h1>Sorting Descending</h1>
-    <?php
-    $cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
-    rsort($cars);
+		<div class="col-3">
+			<h1>Sorting Descending</h1>
+			<?php
+			$cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
+			rsort($cars);
 
-    $clength = count($cars);
-    for($x = 0; $x < $clength; $x++) {
-        echo $cars[$x];
-        echo "<br>";
-    }
-    ?>
-  </div>
-</div>
+			$clength = count($cars);
+			for($x = 0; $x < $clength; $x++) {
+				echo $cars[$x];
+				echo "<br>";
+			}
+			?>
+		</div>
+	</div>
 
 
 
