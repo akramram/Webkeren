94089122383d7d70ff2cadb2ab743dd5afd528d6
ata45 <34927815+ata45@users.noreply.github.com>
2018-10-23 15:17:59 +0700

    edit arit

--- aritmatika.php
+++ aritmatika.php
@@ -28,15 +28,15 @@
 	?>
 	<div class="kalkulator">
 		<h2 class="judul">KALKULATOR</h2>
-		<form method="post" action="index.php">			
+		<form method="post" action="aritmatika.php">			
 			<input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama">
-			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
 			<select class="opt" name="operasi">
 				<option value="tambah">+</option>
 				<option value="kurang">-</option>
 				<option value="kali">x</option>
 				<option value="bagi">/</option>
 			</select>
+			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
 			<input type="submit" name="hitung" value="Hitung" class="tombol">											
 		</form>
 		<?php if(isset($_POST['hitung'])){ ?>
