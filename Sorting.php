b18dbbec8d01f19a83a53ccc289eff62b20a4842
chowderlatte <32903348+chowderlatte@users.noreply.github.com>
2018-10-23 21:11:11 +0700

    Sorting Sudah gege

--- Sorting.php
+++ Sorting.php
@@ -2,42 +2,117 @@
 <html>
 <head>
 	<title>Sorting</title>
-  <link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-	<div class="col-12">
-  <div class="col-3" style="float:left;">
-    <h1>Sorting Ascending</h1>
+	<?php
 
-      <?php
-      $cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
-      sort($cars);
+	  if(isset($_POST['btnOk'])){
 
+	  $nama= $_POST['nama'];
 
-			$clength = count($cars);
-			for($x = 0; $x < $clength; $x++) {
-				echo $cars[$x];
-				echo "<br>";
-			}
-			?>
-		</div>
-  <div class="col-3" style="float:left;">
-    <h1>Sorting Descending</h1>
-    <?php
-    $cars = array("Volvo", "BMW", "Toyota","xride","tamiya" ,"Avanza");
-    rsort($cars);
+	  foreach($nama as $key => $val){
 
+	  echo 'Data ke - ' .$key. ' = '.$nama[$key].' <br/>';
 
-			$clength = count($cars);
-			for($x = 0; $x < $clength; $x++) {
-				echo $cars[$x];
-				echo "<br>";
-			}
-			?>
-		</div>
-	</div>
+	  }
 
+	  }
 
+	  ?>
 
+	<form method="get" name="frm" action="">
+
+	  <input name="jumlah" type="text" />
+
+	  <input type="submit" name="btnJumlah" value="Ok" />
+
+	  </form>
+
+	<form method="post" name="frmpost" action="">
+
+	  <table width="547" border="0" cellpadding="0" cellspacing="0">
+
+	  <!--DWLayoutTable-->
+
+	  <tr>
+
+	  <td width="32" height="22" valign="top">No</td>
+
+	  <td width="240" valign="top">Isi Array</td>
+
+	  </tr>
+
+	  <?php
+
+	  if(isset($_GET['jumlah']) && $_GET['jumlah']>0){
+
+	  $jumlah_form = $_GET['jumlah'];
+
+	  }
+
+	  else{
+
+	  $jumlah_form = 1;
+
+	  }
+
+	  for($i=1; $i<=$jumlah_form; $i++){
+
+	  ?>
+
+	  <tr>
+
+	  <td height="23"><?php echo $i; ?></td>
+
+	  <td><input name="nama[]" type="text" size="30" /></td>
+
+	  </tr>
+
+	  <?php
+
+	  }
+
+	  ?>
+
+	  <tr>
+
+	  <td height="23" colspan="4" align="right"><input type="submit" name="btnOk" value="Simpan" /></td>
+
+	  </tr>
+
+	  </table>
+
+	  </form>
+
+<h1>Sorting Ascending</h1>
+<?php
+if (empty($nama)) {
+	echo "Data Kosong";
+}else {
+	sort($nama);
+	$arrlength=count($nama);
+	for($x=0;$x<$arrlength;$x++)
+	  {
+	  echo $nama[$x];
+	  echo "<br>";
+	  }
+}
+?>
+
+<h1>Sorting Descending</h1>
+<?php
+if (empty($nama)) {
+	echo "Data Kosong";
+}else {
+	rsort($nama);
+	$arrlength=count($nama);
+	for($x=0;$x<$arrlength;$x++)
+	  {
+	  echo $nama[$x];
+	  echo "<br>";
+	  }
+}
+?>
 </body>
 </html>
